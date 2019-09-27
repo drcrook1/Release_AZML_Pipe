e4c05f6afd84532af510befd5092ffa61a7885bf
David Crook <dacrook@microsoft.com>
2019-09-27 11:34:26 -0400

    updated with a working arm template

---
+++ deploy_environment/00_ml_arm_deploy.ps1
@@ -0,0 +1,34 @@
+$resourceGroupName = "dacrook-test-arm"
+$location = "eastus"
+$templateParamObject = @{
+    "baseName" = "dacrook"
+    "clusterAdminUserName" = "dacrook123"
+    "clusterAdminUserPassword" = "D@vid!234567890"
+    "clusterName" = "cpu"
+    "minNodeCount" = 0
+}
+$parameters = @{
+    "Name" = "dacrookamlarmtest"
+    "ResourceGroupName" = $resourceGroupName
+    "TemplateFile" = "./arm_templates/00_ml_template.json"
+    "TemplateParameterObject" = $templateParamObject
+}
+Login-AzureRmAccount
+Select-AzureRmSubscription -SubscriptionName "jayoung-Team Subscription"
+
+#Create or check for existing resource group
+$resourceGroup = Get-AzureRmResourceGroup -Name $resourceGroupName -ErrorAction SilentlyContinue
+if(!$resourceGroup)
+{
+    Write-Host "Creating resource group '$resourceGroupName' in location '$location'";
+    New-AzureRmResourceGroup -Name $resourceGroupName -Location $location
+}
+else{
+    Write-Host "Using existing resource group '$resourceGroupName'";
+}
+
+#Start Actual Deployment
+New-AzureRmResourceGroupDeployment -ResourceGroupName $resourceGroupName `
+                                    -Mode Incremental `
+                                    -TemplateFile "./arm_templates/00_ml_template.json" `
+                                    -TemplateParameterObject $templateParamObject
